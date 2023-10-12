1e34783bb5247f3cb2149286a9621b0fcace4fce
Nick Mt <ndplay@ymail.com>
2023-10-12 08:04:49 +0400

    Upcoming logger
    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -10,7 +10,6 @@ class Schedule:
         self.last_modified = os.path.getmtime(self.filename)
 
     def load_schedule(self):
-        # Ensure file exists, otherwise, create
         if not os.path.exists(self.filename):
             with open(self.filename, "w") as file:
                 file.write("")
@@ -24,36 +23,40 @@ class Schedule:
                 schedule.append((start_time.strip(), task_detail.strip()))
             return schedule
 
-    def get_current_task(self):
+    def get_current_and_next_task(self):
         current_time = datetime.now().time()
-        current_task = "No task scheduled"
+        current_task = ("00:00", "No task scheduled")
+        next_task = ("00:00", "No upcoming task")
         for i, (start_time, task_detail) in enumerate(self.schedule):
             task_time = datetime.strptime(start_time, "%H:%M").time()
             if task_time <= current_time:
-                current_task = task_detail
+                current_task = (start_time, task_detail)
+                if i < len(self.schedule) - 1:
+                    next_task = self.schedule[i + 1]
             else:
                 break
 
-        return current_task
+        return current_task, next_task
 
     def check_for_updates(self):
         modified_time = os.path.getmtime(self.filename)
         if modified_time > self.last_modified:
-            print("Schedule updated, reloading...")
+            print(f"[{datetime.now().strftime('%H:%M')}] [UPDATE] Schedule updated, reloading...")
             self.schedule = self.load_schedule()
             self.last_modified = modified_time
 
 def main():
     schedule = Schedule("schedule.txt")
-    current_task = ""
+    current_task = ("", "")
     while True:
         schedule.check_for_updates()
-        task = schedule.get_current_task()
+        task, next_task = schedule.get_current_and_next_task()
         if current_task != task:
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            if current_task:
-                print(f"{current_time} - END   : {current_task}")
-            print(f"{current_time} - START : {task}")
+            current_time = datetime.now().strftime("%H:%M")
+            if current_task[1]:
+                print(f"[{current_time}] [END]   : {current_task[1]}")
+            print(f"[{current_time}] [START] : [{task[0]}] {task[1]}")
+            print(f"[{current_time}] [NEXT]  : [{next_task[0]}] {next_task[1]}")
             current_task = task
         time.sleep(60)
 
